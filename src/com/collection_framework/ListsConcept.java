3cb803bf43b36c5e5e9358308ae84695149eefe2
fuadik88 <fuadik88@gmail.com>
2021-07-29 22:17:52 +0400

    INITIAL COMMIT

--- src/com/collection_framework/ListsConcept.java
+++ src/com/collection_framework/ListsConcept.java
@@ -1,5 +1,12 @@
 package com.collection_framework;
 
-public class ListsConcept {
+import java.util.ArrayList;
 
+public class ListsConcept {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.ensureCapacity(100);  //set the size of Arraylist for incoming bulk of data
+        list.trimToSize();      //delete all empty fields to trim the Arraylist
+
+    }
 }

e4f09cd87001e9ab5c48baada036f7de8aa7f135
fuadik88 <fuadik88@gmail.com>
2021-07-29 22:29:55 +0400

    INITIAL COMMIT

--- src/com/collection_framework/ListsConcept.java
+++ src/com/collection_framework/ListsConcept.java
@@ -7,6 +7,11 @@ public class ListsConcept {
         ArrayList<String> list = new ArrayList<>();
         list.ensureCapacity(100);  //set the size of Arraylist for incoming bulk of data
         list.trimToSize();      //delete all empty fields to trim the Arraylist
-        System.out.println(list.size());
+        System.out.println(list.size());    //size of Arraylist
+        System.out.println(list.isEmpty()); //returns boolean to say if Arraylist is empty
+        list.clear(); //clear all elements of Arraylist
+
+        String[] names = list.toArray(new String[0]); //convert Arraylist to Array.
+
     }
 }

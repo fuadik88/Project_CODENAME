63492e07e7f28f464637c534958fd3bacc9a5c0f
fuadik88 <fuadik88@gmail.com>
2021-07-30 19:32:57 +0400

    INITIAL COMMIT
    Signed-off-by: fuadik88 <fuadik88@gmail.com>

--- src/com/collection_framework/LinkedListUsage.java
+++ src/com/collection_framework/LinkedListUsage.java
@@ -1,8 +1,17 @@
 package com.collection_framework;
 
+import java.util.LinkedList;
+
 public class LinkedListUsage {
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        gen(list);
+        System.out.println(list);
 
+    }
 
+    private static void gen(LinkedList<Integer> list){
+        for(int i=0; i<10; i++)
+            list.add((int)(Math.random()*20+1));
     }
 }
